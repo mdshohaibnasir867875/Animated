7c34f0b182fa39dba7e3311e024d481744c78d2d
mdshohaibnasir867875 <mdshohaibnasir867875@gmail.com>
2022-12-22 10:14:04 +0600

    first commit

---
+++ nns.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+    float r, area;
+    printf("Enter the value of r");
+    scanf("%f", &r);
+    area=3.14 *r*r
+    printf("Area=%f", area);
+}
